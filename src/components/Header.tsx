791eeab89f92670a2298051ac864f982178411f0
qbreis <qbreis@gmail.com>
2023-03-07 16:50:57 +0100

    feat: add auth0 package

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
 export default function Header() {
+    const { isAuthenticated, logout } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
     return (
         <header>
             <h1>React Auth0</h1>
-            <nav>
-                
-            </nav>
+            <nav>{isAuthenticated ? <>Sign Out</> : <>Sign In</>}</nav>
         </header>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,25 @@
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import "./index.css";
+
+const domain:string = process.env.REACT_APP_AUTH0_DOMAIN!; // Use type string + non-null assertion operator !
+const clientId:string = process.env.REACT_APP_AUTH0_CLIENT_ID!; // Use type string + non-null assertion operator !
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+            redirect_uri: window.location.origin,
+        }}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+);
+
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,3 +39,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
