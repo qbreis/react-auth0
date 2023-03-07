67ad1784ac2fe653864f92aeccf48a545c50244c
qbreis <qbreis@gmail.com>
2023-03-07 13:31:29 +0100

    chore: initial

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>React Auth0</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
